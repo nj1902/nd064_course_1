d8767fab47fe2255651452866b8a74d43e23b3c9
nj1902 <1902ben10@gmail.com>
2021-07-11 15:04:01 +0530

    application logging

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 import json
+import logging
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    # log line
+    app.logger.info("Main request successfull.")
     return "Hello World!"
 
 @app.route("/status")
@@ -13,6 +16,8 @@ def healthcheck():
         status = 200,
         mimetype = 'application.json',
     )
+    # log line
+    app.logger.info("Status request successfull.")
     return response
 
 @app.route("/metrics")
@@ -23,7 +28,13 @@ def metrics():
             "UserCountActive": 23
             }
     }
+    # log line
+    app.logger.info("Metrics request successfull.")
     return context
 
 if __name__ == "__main__":
+
+    # stream logs to app.log file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
     app.run(host='127.0.0.1')
