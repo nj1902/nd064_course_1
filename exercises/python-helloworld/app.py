2cbbe0b6532ae1192f634ee4b70b0f344d5be9d5
nj1902 <1902ben10@gmail.com>
2021-07-11 15:03:34 +0530

    endpoints of application

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,29 @@
 from flask import Flask
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/status")
+def healthcheck():
+    response = app.response_class(
+        response = json.dumps({"result": "OK - healthy"}),
+        status = 200,
+        mimetype = 'application.json',
+    )
+    return response
+
+@app.route("/metrics")
+def metrics():
+    context = {
+        "data": {
+            "UserCount": 14,
+            "UserCountActive": 23
+            }
+    }
+    return context
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='127.0.0.1')
